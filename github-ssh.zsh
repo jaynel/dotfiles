27dca7567250367a72f96c79421c6af834650337
Tim Holland <th0114nd@gmail.com>
2014-07-25 00:11:51 -0700

    Removed prompting

--- github-ssh.zsh
+++ github-ssh.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-ssh-keygen -t rsa -C "th0114nd@gmail.com"
+echo "\n\n\n" | ssh-keygen -t rsa -C "th0114nd@gmail.com" -f ~/.ssh/id_rsa
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 pbcopy < ~/.ssh/id_rsa.pub
